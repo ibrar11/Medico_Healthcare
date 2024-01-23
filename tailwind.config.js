72ccfaad657033743f65c0c2ce6ca195362e50c7
Ibrar Munawar <ibrarmunawar0@gmail.com>
2024-01-23 20:51:03 +0500

    added colors and set the container

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+    colors: {
+      Primary: '#00A0AA',
+      Secondary: "#FFC567",
+      C_grey: "#D9D9D9",
+      Light_Grey:" #979797"
+    }
+  },
+  plugins: [],
+}
+
