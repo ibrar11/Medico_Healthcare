d7d863094f56c6bb76da379a615b4efc46274926
Ibrar Munawar <ibrarmunawar0@gmail.com>
2024-01-30 12:02:57 +0500

    adjusts the image radius and sub card positions

--- src/components/TreatmentCard.jsx
+++ src/components/TreatmentCard.jsx
@@ -8,8 +8,8 @@ const TreatmentCard = ({
 }) => {
   return (
     <div className={`relative sm:max-h-64 sm:max-w-80`}>
-      <img className="h-full w-full" src={image} alt={alt} />
-      <div className="absolute -bottom-9 left-10  flex flex-col items-start justify-center gap-y-1 rounded-lg bg-primary px-8 pb-4 pt-2 text-white lg:-bottom-8 lg:left-10">
+      <img className="h-full w-full rounded-lg" src={image} alt={alt} />
+      <div className="absolute -bottom-9 left-10  flex flex-col items-start justify-center gap-y-1 rounded-lg bg-primary px-8 pb-4 pt-2 text-white lg:-bottom-14 lg:left-10 lg:pb-8 lg:pt-5">
         <p className="text-xs font-bold lg:text-sm">{title}</p>
         <p className="text-base font-bold lg:text-lg">{profession}</p>
       </div>
